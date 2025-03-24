dbe5bbe6175bbf9c56f207c6622fd80c7cdb0536
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2025-03-24 01:34:16 -0500

    v0.3 implemented basic Bun server with API routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,18 @@
-Bun.serve({
-    port: Bun.env.BACKEND_PORT,
-    async fetch(request) {
-        return new Response('Hello, World!');
-    },
+const server = Bun.serve({
+    // entorno de desarrollo
+	development: true,
+	// puerto de ejecución
+	port: parseInt(Bun.env.BACKEND_PORT || '4000'),
+	// rutas de la API
+	routes: {
+		'/api/': () => new Response('REST API root', { status: 201 }),
+		'/': () => new Response('root', { status: 201 }),
+	},
+
+	fetch(request) {
+		// 404 no encontrado
+		return new Response('404: NOT FOUND', { status: 404 });
+	},
 });
+
+console.log(`Server ejecutando en puerto ${server.port}`);
